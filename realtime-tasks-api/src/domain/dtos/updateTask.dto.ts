f076c2438529930a077515a90bfe030849fbff89
Reinaldo <reinaldo.bustamante026@gmail.com>
2025-05-29 08:22:35 -0400

    refactor: update constructor parameters for better control in PUT method

--- realtime-tasks-api/src/domain/dtos/updateTask.dto.ts
+++ realtime-tasks-api/src/domain/dtos/updateTask.dto.ts
@@ -1,17 +1,21 @@
 
 export class UpdateTaskDto {
     constructor(
-        public status: string,
-        public updated_at: Date
+        public title?: string,
+        public description?: string,
+        public status?: string,
+        public updated_at?: Date
     ){}
 
     public static create(object: {[key:string]: any}): [string?, UpdateTaskDto?] {
-        const { status } = object
-        const allowedStatuses = ['pending', 'completed']
+        const { title, description, status } = object
+        const allowedStatuses = ['pendiente', 'completada']
         const date = new Date()
-        if ( !status ) return ['Missing status']
-        if( !allowedStatuses.includes(status) ) return ['Invalid value for status. Only pending or completed are allowed.']
+
+        if(title && title.length > 100) return ["title must be at most 100 characters long"]
+        if(description && description.length > 500) return ["description must be at most 500 characters long"]
+        if(status && !allowedStatuses.includes(status) ) return ['Invalid value for status. Only "pendiente" or "completada" are allowed.']
         
-        return [undefined, new UpdateTaskDto(status, date)]
+        return [undefined, new UpdateTaskDto(title, description, status, date)]
     }
 }
